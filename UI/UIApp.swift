4115dd6c45d267f46df4988519b49bbea9bca9e6
sofia <sofigerenacortes@gmail.com>
2023-06-23 12:12:44 -0500

    Initial Commit

---
+++ UI/UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIApp.swift
+//  UI
+//
+//  Created by Monitor #5 on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
